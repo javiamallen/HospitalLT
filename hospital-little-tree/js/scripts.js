37e3e1f74703147fbe2aff3277e4a92c0b77fcdd
javiamallen <javierafulls@gmail.com>
2024-11-17 00:57:32 -0300

    sass compilado + bootstrap nwk

---
+++ hospital-little-tree/js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", () => {
+    // Ejemplo: Mostrar un mensaje en consola al cargar la página
+    console.log("Página cargada correctamente");
+  });
+  
